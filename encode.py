38d3ea0a9c27c84cad738e28e04483e959916b99
joemon-at <joeashthomas@gmail.com>
2026-03-02 17:47:04 +0530

    error handling in encode/main

--- encode.py
+++ encode.py
@@ -4,48 +4,49 @@ import os
 
 def encode(img_location):
     #get data of image
-    
-    msg_to_hide = input('enter message to hide: ')
+    if(os.path.exists(img_location) == True):
+        msg_to_hide = input('enter message to hide: ')
 
-    #process image
-    image = Image.open(img_location,'r')
-    width, height = image.size
+        #process image
+        image = Image.open(img_location,'r')
+        width, height = image.size
 
-    img_arr = np.array(list(image.get_flattened_data()))
+        img_arr = np.array(list(image.get_flattened_data()))
 
 
-    if image.mode == "P":
-        print("Not supported")
-        exit()
+        if image.mode == "P":
+            print("Not supported")
+            exit()
 
-    channels = 4 if image.mode == "RGBA" else 3
-    pixels = img_arr.size // channels
-    stop_indicator = "$ALL DONE$"
-    stop_indicator_len = len(stop_indicator)
+        channels = 4 if image.mode == "RGBA" else 3
+        pixels = img_arr.size // channels
+        stop_indicator = "$ALL DONE$"
+        stop_indicator_len = len(stop_indicator)
 
-    msg_to_hide += stop_indicator
+        msg_to_hide += stop_indicator
 
-    #convert the message into bytes
-    message_byte = ''.join(f"{ord(c):08b}" for c in msg_to_hide)
-    bits = len(message_byte)
+        #convert the message into bytes
+        message_byte = ''.join(f"{ord(c):08b}" for c in msg_to_hide)
+        bits = len(message_byte)
 
-    if bits>pixels:
-        print("Not enough space in miage\n " \
-        "Choose a smaller sentence please")
+        if bits>pixels:
+            print("Not enough space in miage\n " \
+            "Choose a smaller sentence please")
+        else:
+            index = 0
+            for i in range(pixels):
+                for j in range(0,3):
+                    if index<bits: 
+                        img_arr[i][j] = int(bin(img_arr[i][j])[2:-1] + message_byte[index],2)
+                        index+=1
+            
+            img_arr = img_arr.reshape((height,width,channels))
+            result = Image.fromarray(img_arr.astype('uint8'), image.mode)
+            result.save("temp.png")
+            return (os.path.abspath("temp.png"))
     else:
-        index = 0
-        for i in range(pixels):
-            for j in range(0,3):
-                if index<bits: 
-                    img_arr[i][j] = int(bin(img_arr[i][j])[2:-1] + message_byte[index],2)
-                    index+=1
-        
-        img_arr = img_arr.reshape((height,width,channels))
-        result = Image.fromarray(img_arr.astype('uint8'), image.mode)
-        result.save("temp.png")
-        return (os.path.abspath("temp.png"))
-
-    #the bit in the 1's place, LSB, is replaced 
-    #with a bit of our hidden message
+        return "Path DNE"
+        #the bit in the 1's place, LSB, is replaced 
+        #with a bit of our hidden message
 
 
--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from encode import encode
 from upload import upload
 import os
 inp_name = input('enter location of image: ')
-out_name = input('enter name of image: ')
+out_name = input('enter name of image\n(file will be saved in S3 using this name): ')
 
 store = encode(inp_name)
-#now a temp file is created, we gotta store its location
-upload(store,out_name)
+if(store == "Path DNE"):
+    print("Invalid image location. Please run again")
+else:
+    #now a temp file is created, we gotta store its location
+    upload(store,out_name)
 
-os.remove(store)
+try:
+
+    if os.path.exists(store):
+        os.remove(store)
+    
+except PermissionError:
+    print(f"unable to delete temp file as permission denied, please delete manually at {store}")
+except OSError as e:
+    print(f"An error occured: {e}")
+
 
 
 #store should be a string, not an image
